12b1ab45d460b770354d9cb132ae9311604638af
emran0335 <emranhossain0335@gmail.com>
2025-12-30 16:44:11 +0600

    nextjs version updated

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -4,6 +4,7 @@ import NextAuth from "next-auth";
 import Nodemailer from "next-auth/providers/nodemailer";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  basePath: '/auth',
   adapter: PrismaAdapter(prisma),
   providers: [
     Nodemailer({
